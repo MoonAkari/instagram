25ffeb9b130910d9e3120190fccbf3886a9e1c48
MoonAkari <103241634+MoonAkari@users.noreply.github.com>
2022-08-06 08:29:35 +0700

    Fix Footer, Create Profile page, API with Redux

--- src/layout/OnepageLayout/OnepageLayout.js
+++ src/layout/OnepageLayout/OnepageLayout.js
@@ -1,16 +1,19 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
+import { useLocation } from 'react-router-dom';
+
 import styles from './OnepageLayout.module.scss';
 import Footer from '../../components/footer/Footer';
 
 const cx = classNames.bind(styles);
 
 function OnepageLayout({ children }) {
+  let location = useLocation();
   return (
     <>
       <div className={cx('container')}>
         <div className={cx('content')}>{children}</div>
-        <footer className={cx('footer')}>{children.type.name === 'Setting' ? <Footer small /> : <Footer />}</footer>
+        <footer className={cx('footer')}>{location.pathname === '/setting' ? <Footer small /> : <Footer />}</footer>
       </div>
     </>
   );
--- src/layout/defaultLayout/DefaultLayout.js
+++ src/layout/defaultLayout/DefaultLayout.js
@@ -1,22 +1,25 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './defaultLayout.module.scss';
+import { useLocation } from 'react-router-dom';
 import Header from '../../components/Header/Header.js';
 import Footer from '../../components/footer/Footer';
 
 const cx = classNames.bind(styles);
 
 function DefaultLayout({ children }) {
+  let location = useLocation();
+
   return (
     <div className={cx('container')}>
       <Header />
       <div className={cx('content')}>{children}</div>
-      {children.type.name === 'Profile' ? (
+      {location.pathname === '/' || location.pathname.includes('/direct') ? (
+        <></>
+      ) : (
         <footer className={cx('footer')}>
           <Footer small />
         </footer>
-      ) : (
-        <></>
       )}
     </div>
   );
